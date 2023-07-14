3448ace2c14efd72a5eab0e57877c92ab18f59d8
Aderinola Opemipo Emmanuel <aderinolaopemipo@gmail.com>
2023-07-14 04:57:14 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+
+var randomImageNumber = "dice" + randomNumber1 + ".png";
+
+var randomImageSource = "images/" + randomImageNumber;
+
+document.querySelectorAll("img")[0].setAttribute("src", randomImageSource);
+
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+var randomImageNumber2 = "dice" + randomNumber2 + ".png";
+
+var randomImageSource2 = "images/" + randomImageNumber2;
+
+document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
+
+console.log(randomImageSource);
+console.log(randomImageSource2);
+
+if (randomImageSource > randomImageSource2) {
+    document.querySelector("h1").innerHTML = "🚩Player 1 wins!";
+}
+else if (randomImageSource2 > randomImageSource) {
+    document.querySelector("h1").innerHTML = "Player 2 wins! 🚩";
+}
+else {
+    document.querySelector("h1").innerHTML = "A tie.Play again! 🎌";
+}
